226aad1b3e215552f4dc18cc666e1bc7fd74b604
nguyenthaiduc <nguyenthaiduc12@gmail.com>
2022-06-24 22:33:06 +0700

    useUser

--- src/composables/useUser.ts
+++ src/composables/useUser.ts
@@ -1,3 +1,21 @@
 import { ref } from "vue";
 import type { Ref } from "vue";
 import { projectAuth } from "@/config/firebase";
+import type firebase from "firebase/app";
+// import { User } from '@firebase/auth-types';
+
+type User = firebase.User
+
+const user: Ref<User | null> = ref(projectAuth.currentUser);
+
+projectAuth.onAuthStateChanged(function (_user) {
+  if (_user) user.value = _user;
+});
+
+async function getUser() {
+   return { user };
+}
+
+export function useUser() {
+  return { getUser };
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import TheWelcome from "@/components/TheWelcome.vue";
+import { useUser } from "@/composables/useUser";
+
+const { getUser } = useUser();
+const { user }  = getUser();
 </script>
 
 <template>
+  <div class="mt-6">{{ user?.displayName }}</div>
   <main>
     <TheWelcome />
   </main>
